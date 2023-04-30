26893a31b9cf8c65894f99a495fe740c1afb58fa
mnkvsoft <mnkvsoft@yandex.ru>
2023-04-30 13:26:47 +0300

    rename SimpleMockServer.ConfigurationProviding => SimpleMockServer.Domain.Configuration

--- src/SimpleMockServer.Domain.Abstractions/IRule.cs
+++
@@ -1,9 +0,0 @@
-//using Microsoft.AspNetCore.Http;
-
-//namespace SimpleMockServer.Domain.Abstractions;
-
-//public interface IRule
-//{
-//    Task WriteResponse(HttpResponse response, HttpRequest request);
-//    bool IsMatch(HttpRequest request);
-//}
