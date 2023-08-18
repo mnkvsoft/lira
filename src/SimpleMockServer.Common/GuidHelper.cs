7afbb4762c24ff82e1c78e6d96137ec5596019c0
mnkvsoft <mnkvsoft@yandex.ru>
2023-08-18 17:26:59 +0300

    add guid tests

--- src/SimpleMockServer.Common/GuidHelper.cs
+++ src/SimpleMockServer.Common/GuidHelper.cs
@@ -4,15 +4,31 @@ public static class GuidHelper
 {
     public static Guid ToGuid(this long value)
     {
-        byte[] guidData = new byte[16];
-        Array.Copy(BitConverter.GetBytes(value), guidData, 8);
-        return new Guid(guidData);
+        byte[] valueBytes = BitConverter.GetBytes(value);
+        byte[] randomBytes = new byte[8];
+        
+        Random.Shared.NextBytes(randomBytes);
+
+        byte[] result = new byte[16];
+        for (int i = 0; i < randomBytes.Length; i++)
+        {
+            result[i * 2] = valueBytes[i];
+            result[i * 2 + 1] = randomBytes[i];
+        }
+        
+        return new Guid(result);
     }
 
     public static long ToLong(this Guid guid)
     {
-        if (BitConverter.ToInt64(guid.ToByteArray(), 8) != 0)
-            throw new OverflowException("Value was either too large or too small for an Int64.");
-        return BitConverter.ToInt64(guid.ToByteArray(), 0);
+        var bytes = guid.ToByteArray();
+        byte[] result = new byte[8];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = bytes[i * 2];
+        }
+        
+        return BitConverter.ToInt64(result);
     }
 }
