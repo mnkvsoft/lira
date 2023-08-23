e5a0405130005e6276764d8edb6c6fd326a25291
mnkvsoft <mnkvsoft@yandex.ru>
2023-08-23 13:36:14 +0300

    add multiline variables

--- src/SimpleMockServer.Domain.Configuration/Variables/VariablesParser.cs
+++ src/SimpleMockServer.Domain.Configuration/Variables/VariablesParser.cs
@@ -1,4 +1,5 @@
-﻿using SimpleMockServer.Common.Extensions;
+﻿using System.Text;
+using SimpleMockServer.Common.Extensions;
 using SimpleMockServer.Domain.Configuration.Rules.ValuePatternParsing;
 using SimpleMockServer.Domain.TextPart;
 using SimpleMockServer.Domain.TextPart.Variables;
@@ -14,29 +15,73 @@ class VariablesParser
         _textPartsParser = textPartsParser;
     }
 
-    public async Task<VariableSet> Parse(IReadOnlyCollection<string> lines, ParsingContext parsingContext, Func<string, ObjectTextParts, Variable> create)
+    public async Task<VariableSet> Parse(IReadOnlyCollection<string> lines, ParsingContext parsingContext,
+        Func<string, ObjectTextParts, Variable> create)
     {
         var all = new VariableSet(parsingContext.Variables);
         var newContext = parsingContext with { Variables = all };
-        
+
         var onlyNew = new VariableSet();
 
+
+        string? variableName = null;
+        var variablePattern = new StringBuilder();
+
+        bool isFirstLine = true;
         foreach (var line in lines)
         {
-            var (name, pattern) = line.SplitToTwoParts(Consts.ControlChars.AssignmentOperator).Trim();
+            if (IsDefineVariable(line))
+            {
+                if (!isFirstLine)
+                {
+                    await AddVariable(variableName, variablePattern);
+                    variablePattern.Clear();
+                }
 
-            if (string.IsNullOrEmpty(name))
-                throw new Exception($"Variable name not defined. Line: {line}");
+                var (name, pattern) = line.SplitToTwoParts(Consts.ControlChars.AssignmentOperator).Trim();
+                variableName = name.TrimStart(Consts.ControlChars.VariablePrefix);
+                variablePattern.Append(pattern);
+            }
+            else
+            {
+                variablePattern.Append((variablePattern.Length > 0 ? Environment.NewLine : "") + line);
+            }
 
-            if (string.IsNullOrEmpty(pattern))
-                throw new Exception($"Variable '{name}' not initialized. Line: {line}");
-
-            var parts = await _textPartsParser.Parse(pattern, newContext );
-            
-            all.Add(create(name, parts));
-            onlyNew.Add(create(name, parts));
+            isFirstLine = false;
         }
 
+        await AddVariable(variableName, variablePattern);
+        
         return onlyNew;
+        
+        async Task AddVariable(string? s, StringBuilder stringBuilder)
+        {
+            if (string.IsNullOrEmpty(s))
+                throw new Exception("Variable name not defined");
+
+            string p = stringBuilder.ToString();
+            if (string.IsNullOrEmpty(p))
+                throw new Exception($"Variable '{s}' not initialized");
+
+            var parts = await _textPartsParser.Parse(p, newContext);
+
+            all.Add(create(s, parts));
+            onlyNew.Add(create(s, parts));
+        }
+    }
+
+    private static bool IsDefineVariable(string line)
+    {
+        if (line.StartsWith(Consts.ControlChars.VariablePrefix))
+        {
+            var idx = line.IndexOf(' ');
+            if (idx is -1 or 1)
+                return false;
+
+            var afterName = line[idx..];
+            return afterName.TrimStart(' ').StartsWith(Consts.ControlChars.AssignmentOperator);
+        }
+
+        return false;
     }
 }
