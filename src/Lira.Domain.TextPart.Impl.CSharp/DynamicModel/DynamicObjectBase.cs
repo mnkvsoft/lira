d2ae9c6f0c177e32011c994f6b7bf959e08672a3
mnkvsoft <mnkvsoft@yandex.ru>
2024-01-13 10:32:43 +0300

    @cache => cache

--- src/Lira.Domain.TextPart.Impl.CSharp/DynamicModel/DynamicObjectBase.cs
+++ src/Lira.Domain.TextPart.Impl.CSharp/DynamicModel/DynamicObjectBase.cs
@@ -6,13 +6,13 @@ public class DynamicObjectBase
     public record Dependencies(IDeclaredPartsProvider DeclaredPartsProvider, Cache Cache);
     
     private readonly IDeclaredPartsProvider _declaredPartsProvider;
-    protected readonly Cache @cache;
+    protected readonly Cache cache;
     protected const string NewLine = Constants.NewLine;
     
     public DynamicObjectBase(Dependencies dependencies)
     {
         _declaredPartsProvider = dependencies.DeclaredPartsProvider;
-        @cache = dependencies.Cache;
+        cache = dependencies.Cache;
     }
 
     public dynamic? GetDeclaredPart(string name, RequestData request)
